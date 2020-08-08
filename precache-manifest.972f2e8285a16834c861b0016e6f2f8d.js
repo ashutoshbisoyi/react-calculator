f46531f71ee647984bc5c697044c4ea30d1a7bb7
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2020-08-08 18:08:12 +0530

    Updates

---
+++ precache-manifest.972f2e8285a16834c861b0016e6f2f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4d76ba6c759c12c32c5daaa8746734",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "1f90d944db1265c3a087",
+    "url": "/react-calculator/static/css/main.6427e61b.chunk.css"
+  },
+  {
+    "revision": "755b24bab5d5275f2561",
+    "url": "/react-calculator/static/js/2.66e3b6a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.66e3b6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f90d944db1265c3a087",
+    "url": "/react-calculator/static/js/main.2cf44b1d.chunk.js"
+  },
+  {
+    "revision": "72af886408c063c08d01",
+    "url": "/react-calculator/static/js/runtime-main.1d543a59.js"
+  }
+]);
